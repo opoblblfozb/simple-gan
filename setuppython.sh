bb1f87c12fd5427646702db6e6b36be474da7791
Nonaka Kenya <k.nonaka@opt.ne.jp>
2021-12-25 15:12:26 +0900

    modify command

--- setuppython.sh
+++ setuppython.sh
@@ -1,3 +1,8 @@
+# optuna用 postgres client
+sudo apt update
+sudo apt install postgresql postgresql-contrib
+
+# python3.9環境の用意
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt-get -y update
 sudo apt-get -y install python3.9
@@ -7,5 +12,12 @@ sudo apt-get -y install python3.9-distutils
 python3.9 -m pip install --upgrade setuptools
 python3.9 -m pip install --upgrade pip
 python3.9 -m pip install --upgrade distlib
-sudo update-alternatives --set python /usr/bin/python3.9
-sudo ln -sf /usr/bin/python /usr/local/bin/python
+
+# pythonでpython3.9.7を呼べるように
+echo export PATH='/usr/bin:$PATH' >> ~/.bashrc
+sudo ln -sf /usr/bin/python3.9 /usr/bin/python
+source ~/.bashrc
+
+
+# python packageの準備
+pip install poetry
